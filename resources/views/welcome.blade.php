d1f045d007af0153ea79d9c51d705c77887b67ed
stanislavKacmarik <s.kacmarik@gmail.com>
2023-03-17 13:07:45 +0100

    removed vite from welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 <form>
